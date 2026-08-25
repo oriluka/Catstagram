7ff04056aa1c1beb9ea2f9acabc2383cd764d47e
oriluka <mariahtato@gmail.com>
2026-08-24 20:33:43 -0400

    update db

--- src/db/createDB.sql
+++ src/db/createDB.sql
@@ -11,29 +11,35 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS cats (
     catID VARCHAR(20) PRIMARY KEY, 
-    ownerID VARCHAR,
+    creatorID VARCHAR(20) NOT NULL, 
+    CONSTRAINT fk_creator
+    FOREIGN KEY (creatorID) REFERENCES users(userID),
+    ownerID VARCHAR(20),
     CONSTRAINT fk_user
     FOREIGN KEY (ownerID) REFERENCES users(userID),
     catName CHAR(16),
     likes CHAR(50),
-    dislikes CHAR (50)
+    dislikes CHAR (50),
     age INT
 )
 
 CREATE TABLE IF NOT EXISTS photos (
     photoID VARCHAR(20) PRIMARY KEY,
-    link VARCHAR,
-    catUID,
+    link VARCHAR NOT NULL,
+    catUID VARCHAR(20) NOT NULL,
     CONSTRAINT fk_catID
-    FOREIGN KEY (catUID) REFERENCES cats(catID)
+    FOREIGN KEY (catUID) REFERENCES cats(catID),
+    posterID NOT NULL,
+    CONSTRAINT fk_userID
+    FOREIGN KEY (posterID) REFERENCES users(userID),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 
 CREATE TABLE IF NOT EXISTS favorites (
-    userID,
+    userID NOT NULL,
     constraint fk_likeuser
     FOREIGN KEY (userID) REFERENCES users(userID),
-    photoID,
+    photoID NOT NULL,
     constraint fk_photoid
     FOREIGN KEY (photoID) REFERENCES photos(photoID),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
